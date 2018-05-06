fb7752f08e242bfea99ffbc50d962938d46b1bb8
BJR Matos <bjrmatos@gmail.com>
2018-05-06 18:14:27 -0500

    update optionsSchema and take options from just from extensions scope (not from fs store scope)

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,5 +1,31 @@
+
 module.exports = {
   'name': 'fs-store-aws-sns-sync',
   'main': 'lib/main.js',
-  'dependencies': [ 'templates', 'fs-store' ]
+  'dependencies': ['templates', 'fs-store'],
+  'optionsSchema': {
+    extensions: {
+      'fs-store': {
+        type: 'object',
+        properties: {
+          sync: {
+            type: 'object',
+            properties: {
+              provider: { type: 'string', enum: ['aws-sns'] }
+            }
+          }
+        }
+      },
+      'fs-store-aws-sns-sync': {
+        type: 'object',
+        properties: {
+          accessKeyId: { type: 'string' },
+          secretAccessKey: { type: 'string' },
+          topic: { type: 'string' },
+          subscription: { type: 'string' },
+          region: { type: 'string' }
+        }
+      }
+    }
+  }
 }
